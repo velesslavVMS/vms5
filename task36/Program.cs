9ffc8fc6a13bab7c3d2df8e249fab93830bd6b8e
Vedamir <grandros1@gmail.com>
2023-01-30 22:48:34 +0800

    Task 36

---
+++ task36/Program.cs
@@ -0,0 +1,25 @@
+﻿void InputArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(0, 100); 
+}
+
+void Output(int[] array)
+{
+int sum=0, k=0;
+while (k<array.Length)
+{
+sum=sum+array[k];
+k=k+2;
+}
+Console.WriteLine("Индекс 1-й нечетной позиции равен 0.");
+Console.WriteLine($"Сумма чисел на нечетных позициях равна: {sum}");
+}
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Output(array);
+
