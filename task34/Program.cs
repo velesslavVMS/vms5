e397ea0c4713bfad9073dd09643742f28cb5cace
Vedamir <grandros1@gmail.com>
2023-01-30 22:42:07 +0800

    Task 34

---
+++ task34/Program.cs
@@ -0,0 +1,23 @@
+﻿void Input(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(100, 1000); 
+}
+void Output(int[] array)
+{
+int count = 0; 
+for (int i = 0; i < array.Length; i++) 
+{   
+  int k = array[i] % 2;
+  if (k==0)
+  count = count+1;
+}
+Console.WriteLine($"Число четных чисел равно: {count}");
+}
+ 
+Console.Write("Введите кол-во элементов массива: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+int[] array = new int[n]; 
+Input(array); 
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]"); 
+Output(array);
